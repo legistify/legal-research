cfc7075b515736a932206ce184cb9f80f8c9eea6
PHANTM_V <vyasdeep.dv@gmail.com>
2016-01-02 22:48:22 +0530

    Added comment2comment post and edit model. TOCOME :Delete function for all.Controller work to begin.

--- application/models/forum_model.php
+++ application/models/forum_model.php
@@ -79,6 +79,27 @@ class Forum_model extends CI_Model
 		}
 	}
 
+	public function comment_comm($comm_id)
+	{
+		$user_id = $this->db->query("SELECT `id` FROM `users` WHERE `username`='$username'")->row_array()['id'];
+		$datetime = date('Y-m-d H:i:s');
+		$data = array('comment'=>$this->input->post('comment'),
+					  'user_id'=>$user_id,
+					  'question_id'=>$ques_id,
+					  'votes'=>0,
+					  'datetime'=>$datetime
+					  );
+		$this->db->insert('comments_c',$data);
+		if($this->db->affected_rows() >0)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
 	
 
 	public function edit_commentq($comment_id)
@@ -92,6 +113,32 @@ class Forum_model extends CI_Model
 						  'datetime'=>$datetime
 						  );
 			$this->db->update('comments_q',$data,array('id'=>$comment_id));
+			if($this->db->affected_rows() >0)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	public function edit_commentc($comment_id)
+	{
+		$query_str= "SELECT users.username FROM `comments_c` JOIN users ON comments_c.user_id=users.id WHERE comments_c.id='$comment_id'";
+		$username = $this->db->query($query_str)->row_array()['username'];
+		if($username = $this->session->userdata('unnamed'))
+		{
+			$datetime = date('Y-m-d H:i:s');
+			$data = array('comment'=>$this->input->post('comment'),
+						  'datetime'=>$datetime
+						  );
+			$this->db->update('comments_c',$data,array('id'=>$comment_id));
 			if($this->db->affected_rows() >0)
 			{
 				return 1;
