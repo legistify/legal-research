acbc559985dec2b5bbbef5d9835b81221bab732f
Rishabh <f2014141@pilani.bits-pilani.ac.in>
2016-01-31 23:01:42 +0530

    Articles fully complete.Yipee! :D

--- legal-research.sql
+++ legal-research.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2016 at 11:38 PM
+-- Generation Time: Jan 31, 2016 at 06:30 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -23,18 +23,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `answers`
+--
+
+CREATE TABLE IF NOT EXISTS `answers` (
+  `id` bigint(20) NOT NULL,
+  `answer` mediumtext NOT NULL,
+  `question_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `votes` int(11) NOT NULL DEFAULT '0',
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `answer`, `question_id`, `user_id`, `votes`, `datetime`) VALUES
+(1, 'TEST_ANSWER.', 1, 2, 0, '0000-00-00 00:00:00'),
+(2, 'TEST_ANSWER.', 1, 3, 0, '0000-00-00 00:00:00'),
+(3, 'TEST_ANSWER.', 2, 1, 0, '0000-00-00 00:00:00'),
+(4, 'TEST_ANSWER.', 2, 2, 0, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `articles`
 --
 
 CREATE TABLE IF NOT EXISTS `articles` (
-  `id` bigint(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `topic` varchar(32) NOT NULL,
   `content` mediumtext NOT NULL,
   `Upvotes` mediumint(9) NOT NULL DEFAULT '0',
   `Downvotes` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `title` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `articles`
@@ -42,9 +67,22 @@ CREATE TABLE IF NOT EXISTS `articles` (
 
 INSERT INTO `articles` (`id`, `topic`, `content`, `Upvotes`, `Downvotes`, `user_id`, `title`) VALUES
 (1, 'agr', 'HI', 0, 0, 1, 'hello'),
-(2, 'anp', 'HI', 2, 1, 1, 'helli'),
+(2, 'anp', 'HI', 10, 5, 1, 'helli'),
 (3, 'agr', 'BYE', 0, 0, 2, 'helle'),
-(4, 'anp', 'blah', 2, 0, 1, 'blahish');
+(4, 'anp', 'blah', 5, 3, 1, 'blahish'),
+(5, 'agr', 'nnnn', 98, 78, 1, 't4tyf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_vote_rel`
+--
+
+CREATE TABLE IF NOT EXISTS `article_vote_rel` (
+  `article_id` int(11) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `up_down` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -67,11 +105,11 @@ CREATE TABLE IF NOT EXISTS `comments_articles` (
 --
 
 INSERT INTO `comments_articles` (`id`, `comment`, `user_id`, `article_id`, `Upvotes`, `Downvotes`, `datetime`) VALUES
-(1, '!stcomment', 1, 2, 3, 4, '2016-01-05 01:21:56'),
-(2, '1st comment', 1, 2, 11, 9, '2016-01-05 01:21:56'),
+(1, '!stcomment', 1, 2, 5, 5, '2016-01-05 01:21:56'),
+(2, '1st comment', 1, 2, 29, 20, '2016-01-05 01:21:56'),
 (5, '2ndcomment', 3, 4, 22, 3, '2016-01-05 01:22:51'),
 (6, '@comment', 3, 4, 5, 55, '2016-01-05 01:22:51'),
-(7, 'qwerty', 2, 2, 0, 0, '2016-01-10 02:50:13'),
+(7, 'qwerty', 2, 2, 1, 1, '2016-01-10 02:50:13'),
 (8, 'qwerty', 2, 2, 0, 0, '2016-01-10 02:50:36');
 
 -- --------------------------------------------------------
@@ -101,6 +139,18 @@ INSERT INTO `comments_questions` (`id`, `comment`, `user_id`, `question_id`, `Up
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment_vote_rel`
+--
+
+CREATE TABLE IF NOT EXISTS `comment_vote_rel` (
+  `comment_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `up_down` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `questions`
 --
 
@@ -121,6 +171,30 @@ CREATE TABLE IF NOT EXISTS `questions` (
 INSERT INTO `questions` (`id`, `title`, `description`, `user_id`, `votes`, `datetime`, `tag`) VALUES
 (1, 'T1', 'hhkdhak', 1, 0, '2016-01-01 23:52:07', 'anp'),
 (2, 'T2', 'kjkhkm', 2, 0, '2016-01-01 23:52:07', 'anp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag_rel`
+--
+
+CREATE TABLE IF NOT EXISTS `tag_rel` (
+  `article_id` int(11) NOT NULL,
+  `Topic_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tag_rel`
+--
+
+INSERT INTO `tag_rel` (`article_id`, `Topic_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 1),
+(3, 2),
+(4, 2),
+(5, 2);
 
 -- --------------------------------------------------------
 
@@ -236,6 +310,12 @@ ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`), ADD KEY `fk_articles` (`user_id`), ADD KEY `fk_topics` (`topic`);
 
 --
+-- Indexes for table `article_vote_rel`
+--
+ALTER TABLE `article_vote_rel`
+  ADD KEY `user_id` (`user_id`), ADD KEY `article_id` (`article_id`);
+
+--
 -- Indexes for table `comments_articles`
 --
 ALTER TABLE `comments_articles`
@@ -248,10 +328,22 @@ ALTER TABLE `comments_questions`
   ADD PRIMARY KEY (`id`), ADD KEY `fk_question` (`question_id`), ADD KEY `fk_user` (`user_id`);
 
 --
+-- Indexes for table `comment_vote_rel`
+--
+ALTER TABLE `comment_vote_rel`
+  ADD KEY `comment_id` (`comment_id`), ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `questions`
 --
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`id`), ADD KEY `fk_users` (`user_id`);
+
+--
+-- Indexes for table `tag_rel`
+--
+ALTER TABLE `tag_rel`
+  ADD KEY `topic` (`Topic_id`), ADD KEY `article` (`article_id`);
 
 --
 -- Indexes for table `topics`
@@ -279,7 +371,7 @@ ALTER TABLE `user_lawyer`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `comments_articles`
 --
@@ -322,6 +414,13 @@ ADD CONSTRAINT `fk_articles` FOREIGN KEY (`user_id`) REFERENCES `user_lawyer` (`
 ADD CONSTRAINT `fk_topics` FOREIGN KEY (`topic`) REFERENCES `topics` (`tag`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `article_vote_rel`
+--
+ALTER TABLE `article_vote_rel`
+ADD CONSTRAINT `fkey_articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fkey_usersid` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `comments_questions`
 --
 ALTER TABLE `comments_questions`
@@ -329,10 +428,24 @@ ADD CONSTRAINT `fk_question` FOREIGN KEY (`question_id`) REFERENCES `questions` 
 ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `comment_vote_rel`
+--
+ALTER TABLE `comment_vote_rel`
+ADD CONSTRAINT `fk_comments` FOREIGN KEY (`comment_id`) REFERENCES `comments_articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fkey_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `questions`
 --
 ALTER TABLE `questions`
 ADD CONSTRAINT `fk_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tag_rel`
+--
+ALTER TABLE `tag_rel`
+ADD CONSTRAINT `fk_article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_topic` FOREIGN KEY (`Topic_id`) REFERENCES `topics` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
